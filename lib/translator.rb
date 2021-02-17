a2cf2396001bdf3858f93e290a941d1388faf80e
Devante Lowery <dlowery2120@gmail.com>
2021-02-17 22:14:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_english_meaning(path, emoticon)
   # code goes here
-    load_library(path).each do |key, value|
+  load_library(path).each do |key, value|
     if value[:japanese] == emoticon
       return key
     end
